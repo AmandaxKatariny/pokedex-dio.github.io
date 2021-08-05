c6f8ac3070917b344e9c72fc48c942f79fd9b4c4
Amanda Katariny <47179917+AmandaxKatariny@users.noreply.github.com>
2021-08-05 10:16:33 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	lintOnSave: false,
-        publicPath: '/pokedex-dio/',
+          publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/',
 	css: {
 		loaderOptions: {
 			sass: {
