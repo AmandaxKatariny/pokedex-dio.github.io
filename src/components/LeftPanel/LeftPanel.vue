2e2426e65ed44639d6ca809bf2a5582aa79b48c7
Amanda Katariny <47179917+AmandaxKatariny@users.noreply.github.com>
2021-08-21 10:12:36 -0300

    feat: list description and config

--- src/components/LeftPanel/LeftPanel.vue
+++ src/components/LeftPanel/LeftPanel.vue
@@ -5,25 +5,25 @@
 </template>
 
 <script>
-import LeftPanelContent from "./LeftPanelContent.vue";
+	import LeftPanelContent from './LeftPanelContent.vue';
 
-export default {
-	name: "LeftPanel",
-	components: {
-		LeftPanelContent
-	}
-};
+	export default {
+		name: 'LeftPanel',
+		components: {
+			LeftPanelContent,
+		},
+	};
 </script>
 
 <style lang="scss" scoped>
-.panel {
-	display: flex;
-	width: 100%;
-	padding: 85px 20px 20px;
+	.panel {
+		display: flex;
+		width: 100%;
+		padding: 85px 20px 20px;
 
-	@media (min-width: $viewport-medium) {
-		width: calc(50% + 20px);
-		padding-right: 60px;
+		@media (min-width: $viewport-medium) {
+			width: calc(50% + 20px);
+			padding-right: 60px;
+		}
 	}
-}
 </style>
