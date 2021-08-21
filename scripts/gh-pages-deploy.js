30efcd6939c18837ba3999f2da0f8201b7a89b93
Amanda Katariny <47179917+AmandaxKatariny@users.noreply.github.com>
2021-08-21 11:43:46 -0300

    Update gh-pages-deploy.js

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -13,10 +13,10 @@ const fs = require('fs');
 		await execa('git', ['--work-tree', folderName, 'add', '--all']);
 		await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
 		console.log('Pushing to gh-pages...');
-		await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+		await execa('git', ['push', 'origin', 'main:gh-pages', '--force']);
 		await execa('rm', ['-r', folderName]);
 		// await execa('git', ['checkout', '-f', 'm']);
-		await execa('git', ['branch', '-D', 'gh-pages']);
+		// await execa('git', ['branch', '-D', 'gh-pages']);
 		console.log('Successfully deployed, check your settings');
 	} catch (e) {
 		// eslint-disable-next-line no-console
